3aed8bb3a1da0673afa39332dfdb566a5e996df0
Sebastien Rabaud <sebastien.rabaud@pa.cotte.fr>
2026-06-26 09:14:43 +0200

    fs ram ok

--- project/apps/he_app/main.cpp
+++ project/apps/he_app/main.cpp
@@ -1,40 +1,245 @@
+/*
+ * Copyright (c) 2016 Intel Corporation.
+ * Copyright (c) 2019-2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * USB Mass Storage sample ported to the Alif E7 DK (RTSS-HE).
+ * Exposes a RAM-backed FAT disk over USB using the new (next) USB device
+ * stack and the Synopsys DWC3 UDC controller.
+ *
+ * Based on zephyr/samples/subsys/usb/mass.
+ */
+
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/drivers/gpio.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/usb/usbd.h>
+#include <zephyr/usb/class/usbd_msc.h>
+#include <zephyr/fs/fs.h>
+#include <stdio.h>
 
-#define GREEN_LED_NODE DT_ALIAS(led0)
+/*
+ * sample_usbd.h (samples/subsys/usb/common) has no extern "C" guard and pulls
+ * in C++-incompatible templated headers, so we declare the helpers it provides
+ * directly with C linkage instead of including it. They are implemented in the
+ * C-compiled sample_usbd_init.c.
+ */
+extern "C" {
+struct usbd_context *sample_usbd_init_device(usbd_msg_cb_t msg_cb);
+struct usbd_context *sample_usbd_setup_device(usbd_msg_cb_t msg_cb);
+}
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(GREEN_LED_NODE, gpios);
+LOG_MODULE_REGISTER(main);
 
-LOG_MODULE_REGISTER(app);
+#if CONFIG_DISK_DRIVER_FLASH
+#include <zephyr/storage/flash_map.h>
+#endif
 
-auto main() -> int
+#if CONFIG_FAT_FILESYSTEM_ELM
+#include <ff.h>
+#endif
+
+#if CONFIG_FILE_SYSTEM_LITTLEFS
+#include <zephyr/fs/littlefs.h>
+FS_LITTLEFS_DECLARE_DEFAULT_CONFIG(storage);
+#endif
+
+#if !defined(CONFIG_DISK_DRIVER_FLASH) && \
+	!defined(CONFIG_DISK_DRIVER_RAM) && \
+	!defined(CONFIG_DISK_DRIVER_SDMMC)
+#error No supported disk driver enabled
+#endif
+
+#define STORAGE_PARTITION		storage_partition
+#define STORAGE_PARTITION_ID		FIXED_PARTITION_ID(STORAGE_PARTITION)
+
+static struct fs_mount_t fs_mnt;
+
+#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
+static struct usbd_context *sample_usbd;
+
+#if CONFIG_DISK_DRIVER_RAM
+USBD_DEFINE_MSC_LUN(ram, "RAM", "Zephyr", "RAMDisk", "0.00");
+#endif
+
+#if CONFIG_DISK_DRIVER_FLASH
+USBD_DEFINE_MSC_LUN(nand, "NAND", "Zephyr", "FlashDisk", "0.00");
+#endif
+
+#if CONFIG_DISK_DRIVER_SDMMC
+USBD_DEFINE_MSC_LUN(sd, "SD", "Zephyr", "SD", "0.00");
+#endif
+
+static int enable_usb_device_next(void)
 {
-  int ret;
+	int err;
 
-  if (!gpio_is_ready_dt(&led))
-  {
-    LOG_ERR("Led not ready\n");
-    return 0;
-  }
+	sample_usbd = sample_usbd_init_device(NULL);
+	if (sample_usbd == NULL) {
+		LOG_ERR("Failed to initialize USB device");
+		return -ENODEV;
+	}
 
-  ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-  if (ret < 0)
-  {
-    LOG_ERR("Led config failed\n");
-    return 0;
-  }
+	err = usbd_enable(sample_usbd);
+	if (err) {
+		LOG_ERR("Failed to enable device support");
+		return err;
+	}
 
-  while (true)
-  {
-    LOG_INF("Blink from HE!");
-    ret = gpio_pin_toggle_dt(&led);
-    if (ret < 0)
-    {
-      LOG_ERR("Led toggle failed");
-      return 0;
-    }
+	LOG_DBG("USB device support enabled");
 
-    k_msleep(500);
-  }
+	return 0;
+}
+#endif /* defined(CONFIG_USB_DEVICE_STACK_NEXT) */
+
+static int setup_flash(struct fs_mount_t *mnt)
+{
+	int rc = 0;
+#if CONFIG_DISK_DRIVER_FLASH
+	unsigned int id;
+	const struct flash_area *pfa;
+
+	mnt->storage_dev = (void *)STORAGE_PARTITION_ID;
+	id = STORAGE_PARTITION_ID;
+
+	rc = flash_area_open(id, &pfa);
+	printk("Area %u at 0x%x on %s for %u bytes\n",
+	       id, (unsigned int)pfa->fa_off, pfa->fa_dev->name,
+	       (unsigned int)pfa->fa_size);
+
+	if (rc < 0 && IS_ENABLED(CONFIG_APP_WIPE_STORAGE)) {
+		printk("Erasing flash area ... ");
+		rc = flash_area_flatten(pfa, 0, pfa->fa_size);
+		printk("%d\n", rc);
+	}
+
+	if (rc < 0) {
+		flash_area_close(pfa);
+	}
+#endif
+	return rc;
+}
+
+static int mount_app_fs(struct fs_mount_t *mnt)
+{
+	int rc;
+
+#if CONFIG_FAT_FILESYSTEM_ELM
+	static FATFS fat_fs;
+
+	mnt->type = FS_FATFS;
+	mnt->fs_data = &fat_fs;
+	if (IS_ENABLED(CONFIG_DISK_DRIVER_RAM)) {
+		mnt->mnt_point = "/RAM:";
+	} else if (IS_ENABLED(CONFIG_DISK_DRIVER_SDMMC)) {
+		mnt->mnt_point = "/SD:";
+	} else {
+		mnt->mnt_point = "/NAND:";
+	}
+
+#elif CONFIG_FILE_SYSTEM_LITTLEFS
+	mnt->type = FS_LITTLEFS;
+	mnt->mnt_point = "/lfs";
+	mnt->fs_data = &storage;
+#endif
+	rc = fs_mount(mnt);
+
+	return rc;
+}
+
+static void setup_disk(void)
+{
+	struct fs_mount_t *mp = &fs_mnt;
+	struct fs_dir_t dir;
+	struct fs_statvfs sbuf;
+	int rc;
+
+	fs_dir_t_init(&dir);
+
+	if (IS_ENABLED(CONFIG_DISK_DRIVER_FLASH)) {
+		rc = setup_flash(mp);
+		if (rc < 0) {
+			LOG_ERR("Failed to setup flash area");
+			return;
+		}
+	}
+
+	if (!IS_ENABLED(CONFIG_FILE_SYSTEM_LITTLEFS) &&
+	    !IS_ENABLED(CONFIG_FAT_FILESYSTEM_ELM)) {
+		LOG_INF("No file system selected");
+		return;
+	}
+
+	rc = mount_app_fs(mp);
+	if (rc < 0) {
+		LOG_ERR("Failed to mount filesystem");
+		return;
+	}
+
+	/* Allow log messages to flush to avoid interleaved output */
+	k_sleep(K_MSEC(50));
+
+	printk("Mount %s: %d\n", fs_mnt.mnt_point, rc);
+
+	rc = fs_statvfs(mp->mnt_point, &sbuf);
+	if (rc < 0) {
+		printk("FAIL: statvfs: %d\n", rc);
+		return;
+	}
+
+	printk("%s: bsize = %lu ; frsize = %lu ;"
+	       " blocks = %lu ; bfree = %lu\n",
+	       mp->mnt_point,
+	       sbuf.f_bsize, sbuf.f_frsize,
+	       sbuf.f_blocks, sbuf.f_bfree);
+
+	rc = fs_opendir(&dir, mp->mnt_point);
+	printk("%s opendir: %d\n", mp->mnt_point, rc);
+
+	if (rc < 0) {
+		LOG_ERR("Failed to open directory");
+	}
+
+	while (rc >= 0) {
+		struct fs_dirent ent = {};
+
+		rc = fs_readdir(&dir, &ent);
+		if (rc < 0) {
+			LOG_ERR("Failed to read directory entries");
+			break;
+		}
+		if (ent.name[0] == 0) {
+			printk("End of files\n");
+			break;
+		}
+		printk("  %c %u %s\n",
+		       (ent.type == FS_DIR_ENTRY_FILE) ? 'F' : 'D',
+		       ent.size,
+		       ent.name);
+	}
+
+	(void)fs_closedir(&dir);
+
+	return;
+}
+
+int main(void)
+{
+	int ret;
+
+	setup_disk();
+
+#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
+	ret = enable_usb_device_next();
+#else
+	ret = usb_enable(NULL);
+#endif
+	if (ret != 0) {
+		LOG_ERR("Failed to enable USB");
+		return 0;
+	}
+
+	LOG_INF("The device is put in USB mass storage mode.\n");
+	return 0;
 }
